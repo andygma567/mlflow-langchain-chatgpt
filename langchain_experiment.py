fd17c1f27c736fb4e763975c233438ce17baa2cc
andygma567 <andygma567@gmail.com>
2023-09-29 23:25:47 +0800

    Update the script to use mlflow
    Unfortunately, there seems to be some kind of bug with the mlflow ui...
    but the code works fine.

--- langchain_experiment.py
+++ langchain_experiment.py
@@ -17,6 +17,9 @@ from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import TokenTextSplitter
 from pyngrok import ngrok
 
+import mlflow
+import pandas as pd
+
 # Set the API key - if this key gets committed to a gitrepo then it gets
 # disabled
 NGROK_AUTH_TOKEN = ""
@@ -24,25 +27,56 @@ ngrok.set_auth_token(NGROK_AUTH_TOKEN)
 MY_API_KEY = ""
 os.environ["OPENAI_API_KEY"] = MY_API_KEY
 
-loader = WebBaseLoader("https://sites.google.com/view/mnovackmath/home")
-docs = loader.load()
+website = "https://sites.google.com/view/mnovackmath/home"
 
-text_splitter = TokenTextSplitter(chunk_size=4000, chunk_overlap=0)
-docs = loader.load_and_split(text_splitter=text_splitter)
-
-print(f"Number of docs: {len(docs)}")
-print()
-print(textwrap.fill(docs[0].page_content, max_lines=10))
-
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-# Instantiate the LLMChain and StuffDocumentsChain
+# Instantiate the LLMChain and text splitter for use later
 prompt = PromptTemplate.from_template("Summarize this content: {context}")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 llm_chain = LLMChain(llm=llm, prompt=prompt)
 stuff_chain = StuffDocumentsChain(llm_chain=llm_chain)
+text_splitter = TokenTextSplitter(chunk_size=4000, chunk_overlap=0)
 
-# Run the chain
-output = stuff_chain.run(docs)
+# Set up the experiment tracking
+mlflow.set_tracking_uri("")
+experiment = mlflow.set_experiment("Langchain + mlflow")
+with mlflow.start_run():
+    # Load the documents
+    loader = WebBaseLoader(website)
+    docs = loader.load_and_split(text_splitter=text_splitter)
 
-print()
-print("ChatGPT output:")
-print(textwrap.fill(output))
+    # Log the number of docs
+    print("Logging the number of docs")
+    params = {
+        "num_docs": len(docs),
+        "website": website,
+    }
+    mlflow.log_params(params)
+
+    # Make prediction
+    inputs = [docs[0].page_content]
+    outputs = [stuff_chain.run(docs[:1])]
+    prompts = [stuff_chain.llm_chain.prompt.template]
+
+    print()
+    print("ChatGPT output:")
+    print(textwrap.fill(outputs[0]))
+
+    # Log prediction
+    print()
+    print("Logging prediction")
+    model_info = mlflow.llm.log_predictions(inputs, outputs, prompts)
+
+    # log the model, I can use the infer signature later if I want
+    logged_model = mlflow.langchain.log_model(
+        llm_chain,
+        "langchain_llm_chain",
+    )
+    # Logging the table artifacts
+    data_dict = {
+        'prompts': prompts,
+        'inputs': inputs,
+        'outputs': outputs,
+    }
+    df = pd.DataFrame(data_dict)
+    mlflow.log_table(data=df, artifact_file="prediction_results.json")
+
